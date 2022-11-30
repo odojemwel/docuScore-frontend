137031a7bf108093092eb488221cba967d2a274b
Jemwel Odo <odojemwel@gmail.com>
2022-11-30 17:15:44 +0800

    modified create class page

--- src/Pages/Class.tsx
+++ src/Pages/Class.tsx
@@ -1,80 +1,90 @@
-import { Button, styled, Typography,Stack } from '@mui/material'
+import { Button, styled, Typography, Stack, Paper, Grid } from '@mui/material'
 import React from 'react'
 import PageContainer from '../Components/PageContainer'
 import Box from '@mui/material/Box';
-import Navigation from '../Components/Navigation'
 import TextField from '@mui/material/TextField';
 
-const SamplePage = () => {
+const Class = () => {
   return (
     <PageContainer logged_in>
-      <Typography variant="h4" sx={{marginLeft:4}}>Create Exam </Typography>
-      <Box
-      
-    sx={{
-        marginTop: 20,
-        width: '60%',
-        height: '%',
-        backgsroundColor: 'background.default',
-        border: 1,
-        borderColor: 'white',
-        mx: 'auto',
-        flexGrow: 1, 
-        boxShadow:4,
-        padding: '20px',
-      }}
-    >
-        <Box sx={{display: 'flex', alignItems: 'center',justifyContent: 'center',  marginTop: 5,}}>
-            <Typography variant="h6" component="div" sx={{marginRight: 2}}> Class Name</Typography>
-            <StyledTextField sx={{marginLeft: 1, width: '40%'}} label=" Class Name" id="outlined-password-input" />
-        </Box>
-
-  
-
-    <Box sx={{display: 'flex', alignItems: 'center',justifyContent: 'center', marginTop: 5}}>
-            <Typography variant="h6" component="div" sx={{marginRight: 1}}> Year Level </Typography>
-            <StyledTextField sx={{marginLeft: 2, width: '40%',}} label=" Input Exam number" id="outlined-read-only-input" />
-        </Box>
-    
-   
-<Box sx={{display: 'flex', alignItems: 'center',justifyContent: 'center', marginTop: 5}}>
-            <Typography variant="h6" component="div" sx={{marginRight: 2}}> Section </Typography>
-            <StyledTextField sx={{marginLeft: 3, width: '40%'}} label=" Input Exam number" id="outlined-password-input" />
-        </Box>
-        <Button variant="contained" sx={{marginLeft: 65, marginTop: 2}}>Cancel</Button>
-        <Box>
-        <Button variant="contained" sx={{marginLeft: 45, marginTop: 2}} color="secondary">Create Class</Button>    
-        </Box>
-        </Box>
-
-
-    </PageContainer>
-    
-    
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100%'
+      }}>
+        <Stack sx={{
+          width: "100%"
+        }}
+          spacing={4}>
+          <Typography variant='h5'>Create Class</Typography>
+          <Paper
+            elevation={6}
+            sx={{
+              width: '700px',
+              padding: '30px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}>
+            <Stack width='100%' spacing={3} marginY='30px'>
+              <Grid container>
+                <Grid item xs={3} display='flex' justifyContent='end' alignItems='center'>
+                  <Typography>Class Name</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <TextField sx={{ marginLeft: 1, width: '80%' }}
+                    id="outlined-password-input"
+                    multiline placeholder='Input Class Name'
+                    size='small' />
+                </Grid>
+              </Grid>
+              <Grid container>
+                <Grid item xs={3} display='flex' justifyContent='end' alignItems='center'>
+                  <Typography>Year Level</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <TextField sx={{ marginLeft: 1, width: '80%' }}
+                    id="outlined-password-input"
+                    multiline placeholder='Input Year Level'
+                    size='small' />
+                </Grid>
+              </Grid>
+              <Grid container>
+                <Grid item xs={3} display='flex' justifyContent='end' alignItems='center'>
+                  <Typography>Section</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <TextField sx={{ marginLeft: 1, width: '80%' }}
+                    id="outlined-password-input"
+                    multiline placeholder='Input Section'
+                    size='small' />
+                </Grid>
+              </Grid>
+              <Box display='flex' justifyContent='center'>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  sx={{
+                    marginX: '10px'
+                  }}>
+                  Create Class
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  sx={{
+                    marginX: '10px'
+                  }}>
+                  Cancel
+                </Button>
+              </Box>
+            </Stack>
+          </Paper>
+        </Stack>
+      </Box >
+    </PageContainer >
   )
 }
 
 
-export default SamplePage
-
-const StyledTextField = styled(TextField)({
-    '& label.Mui-focused': {
-      color: '#000000',
-    },
-    '& .MuiInput-underline:after': {
-      borderBottomColor: '#76C8FF',
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: '#76C8FF',
-        borderWidth: '2px'
-      },
-      '&:hover fieldset': {
-        borderColor: '#76C8FF',
-        borderWidth: '2px'
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#76C8FF',
-      },
-    },
-  });
+export default Class
