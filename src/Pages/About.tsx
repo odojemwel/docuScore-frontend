3a4667fc0a21fbba33b29c4f8a5ed6ace5faa30e
i8Zhy <106739036+i8Zhy@users.noreply.github.com>
2022-12-16 12:27:34 +0800

    Contact us & About us

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,6 +1,9 @@
-import { Typography } from '@mui/material'
+import { Button, Typography, Stack, Paper, Grid, Switch, TextField, Box } from '@mui/material'
 import React, { useContext, useEffect } from 'react'
 import PageContainer, { PageContainerContext } from '../Components/PageContainer'
+import tm from './images/tm.png'
+import image from './images/image.png'
+
 
 const About = () => {
   const NavContext = useContext(PageContainerContext)
@@ -9,7 +12,65 @@ const About = () => {
   }, [])
   return (
     <>
-      <Typography> About Us</Typography>
+    <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100%'
+      }}>
+        <Stack sx={{
+          width: "100%"
+        }}
+          spacing={4}>
+        <Grid container justifyContent='center' alignItems={'center'}>
+          <Grid item >
+          <Typography variant='h5'>About docuScore</Typography>
+          </Grid>
+        </Grid>
+          <Paper
+            elevation={6}
+            sx={{
+              width: '1000px',
+              padding: '30px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              height: '669px'
+            }}>
+               <Grid item xs={3} display='flex'  justifyContent='end' alignItems='center'>
+                 {/* <div className='root'>docuScore®</div> */}
+                 <Typography style={{ fontWeight: 'bolder', fontSize: '50px' }}> docuScore® </Typography>
+                </Grid>
+
+            <Stack width='100%'  alignItems='center' spacing={3} marginY='15px'>
+                <Grid item xs={15} display='flex'  >
+                 <Typography style={{textAlign: "justify"}} >Docuscore is a solution concieved by team vVv to contribute to the growing number of IT solutions in the Educationsector. Docuscore aims to help secondary school teachers by letting them scan the scores of their
+                  students and save it anytime to their class record sheet. A big help from the hassle of traditional inputting of scores.</Typography>
+                </Grid>
+                
+                <Grid item  display='flex'  justifyContent='center' alignItems='center'>
+                   <img src={image} height ={240} width={413}/> 
+                </Grid>
+
+                <Grid item xs={3} display='flex'  justifyContent='center' alignItems='center' marginY='18px'>
+                <Typography style={{ fontWeight: 'bolder', fontSize: '20px' }}>The Team vVv</Typography>
+                </Grid>
+
+                <Grid item  display='flex' marginY='20px' justifyContent='center' alignItems='center' >
+                <Typography style={{textAlign: "justify", paddingBottom: '0px'}}>The team is composed of 4 third year IT students from the Cebu Institute of Technology - University. Clied Marky
+                  Ebarbia, April Gem Jagmoc, Jemwel Odo, and Zhiro Javier Pineda are passionate about leveraging the power of our </Typography>
+                </Grid>
+                
+                <Grid item  display='flex' marginY='20px' justifyContent='center' alignItems='center' >
+                <Typography style={{textAlign: "justify", paddingTop: '-30px',marginTop: '-22px'}}> technology in looking for solutions to our daily problems. </Typography>
+                </Grid>
+
+            
+            
+              
+            </Stack>
+          </Paper>
+        </Stack>
+      </Box >
     </>
   )
 }
--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,5 +1,9 @@
-import { Typography } from '@mui/material'
+import { Button, Typography, Stack, Paper, Grid, Switch, TextField } from '@mui/material'
 import { useContext, useEffect } from 'react'
+import Box from '@mui/material/Box';
+import email from './images/email.png'
+import location from './images/location.png'
+import phone from './images/phone.png'
 import { PageContainerContext } from '../Components/PageContainer'
 
 const Contact = () => {
@@ -9,7 +13,67 @@ const Contact = () => {
   }, [])
   return (
     <>
-      <Typography>Contact Us</Typography>
+    <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100%'
+      }}>
+        <Stack sx={{
+          width: "100%"
+        }}
+          spacing={4}>
+        <Grid container justifyContent='center' alignItems={'center'}>
+          <Grid item >
+          <Typography variant='h5'>Contact docuScore</Typography>
+          </Grid>
+        </Grid>
+          <Paper
+            elevation={6}
+            sx={{
+              width: '750px',
+              padding: '30px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}>
+            <Grid container justifyContent='center' alignItems={'center'}>
+              <Grid item >
+                <Typography>Have some questions? Talk to us. We’d love to hear from you</Typography>
+              </Grid>
+            </Grid>
+            <Stack width='100%' spacing={3} marginY='30px'>
+              <Grid container>
+                <Grid item xs={3} display='flex'  justifyContent='end' alignItems='center'>
+                  <img src={location} height ={100} width={100}/>
+                </Grid>
+                <Grid item xs={8} marginY='40px' >
+                <Typography>N. Bacalso Ave., Cebu City Cebu, Philippines 6000</Typography>
+                </Grid>
+              </Grid>
+              <Grid container>
+                <Grid item xs={3} display='flex'  justifyContent='end' alignItems='center'>
+                  <img src={phone} height ={98} width={105}/>
+                </Grid>
+                <Grid item xs={8} marginY='20px' >
+                <Typography>+63 (32) 1234567'</Typography>
+                </Grid>
+              </Grid>
+
+              <Grid container>
+                <Grid item xs={3} display='flex'  justifyContent='end' alignItems='center'>
+                  <img src={email} height ={95} width={95}/>
+                </Grid>
+                <Grid item xs={8} marginY='20px' >
+                <Typography>customercare@docuScore.com</Typography>
+                </Grid>
+              </Grid>
+            
+            
+              
+            </Stack>
+          </Paper>
+        </Stack>
+      </Box >
     </>
   )
 }
